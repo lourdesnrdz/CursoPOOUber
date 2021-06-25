5ffbed24691755bc2563fefea76ddcac02360c5b
lourdesnrdz <lourdesnrdz@gmail.com>
2021-06-24 23:03:40 -0500

    metodo constructor en java y javascript

--- Java/Main.java
+++ Java/Main.java
@@ -1,24 +1,14 @@
 package Java;
 
-public class Main {
+class Main {
     public static void main(String[] args) {
         System.out.println("Hola Mundo");
-
-        Car car = new Car();
-        car.license = "AMQ123";
-        car.driver = "Andres Herrera";
-        car.passenger = 4; 
+        Car car = new Car("AMQ123", new Account("Andres Herrera", "AND123"));
+        car.passenger = 4;
         car.printDataCar();
 
-        Car car2 = new Car();
-        car2.license = "QWE567";
-        car2.driver = "Andrea Herrera";
+        Car car2 = new Car("QWE567", new Account("Andrea Herrera", "ANDA876"));
         car2.passenger = 3;
         car2.printDataCar();
-
-        // Account account = new Account();
-        // Payment payment = new Payment();
-        // Route route = new Route();
-
     }
 }
