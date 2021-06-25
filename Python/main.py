7bfb24e71982b58d552f2d067f420fc0acc0df6e
lourdesnrdz <lourdesnrdz@gmail.com>
2021-06-25 00:09:55 -0500

    herencia en java y php

--- Python/main.py
+++ Python/main.py
@@ -1,10 +1,13 @@
-
 from car import Car
-from account import Account
 
 if __name__ == "__main__":
     print("Hola Mundo")
-    
-    car = Car("AMS234", Account("Andres Herrera", "ANDA876"))
+    car = Car()
+    car.license = "AMS234"
+    car.driver = "Andres Herrera"
     print(vars(car))
-    print(vars(car.driver))
+
+    car2 = Car()
+    car2.license = "QWE567"
+    car2.driver = "Matha"
+    print(vars(car2))
