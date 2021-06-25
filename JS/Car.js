75fc47ca376dfed81ea10c991449823a0bd20acf
lourdesnrdz <lourdesnrdz@gmail.com>
2021-06-24 23:08:57 -0500

    actualizacion javascript

--- JS/Car.js
+++ JS/Car.js
@@ -1,12 +1,14 @@
-function Car(license, drive) {
-    this.id;
-    this.license = license;
-    this.driver = drive;
-    this.passenger;
-}
-
-Car.prototype.printDataCar = function () {
-    console.log(this.license)
-    console.log(this.driver.name)
-    console.log(this.driver.document)
+class Car {
+    constructor(license, drive) {
+        this.id;
+        this.license = license;
+        this.driver = drive;
+        this.passenger;
+    }
+    
+    printDataCar() {
+        console.log(this.license)
+        console.log(this.driver.name)
+        console.log(this.driver.document)
+    }
 }
